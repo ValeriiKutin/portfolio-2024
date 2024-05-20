22a449f29074658f95f84182deb09768a089ab30
Valerii <valerabulon@gmail.com>
2024-05-20 14:32:21 +0300

    test with skeletool

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,43 +1,8 @@
-import PortfolioCategory from "@/components/portfolio-item/PortfolioCategory";
-import "./index.css";
-
-const Portfolio = () => {
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
-  return (
-    <section className="portfolio">
-      <div className="portfolio-wrapper">
-        <PortfolioCategory category="react" title="React" />
-        <PortfolioCategory category="javascript" title="JavaScript" />
-        <PortfolioCategory category="html" title="HTML" />
-      </div>
-    </section>
-  );
-};
-
-export default Portfolio;
-// "use client";
-// import { useState, useEffect } from "react";
 // import PortfolioCategory from "@/components/portfolio-item/PortfolioCategory";
-// import Loading from "./loading";
 // import "./index.css";
 
-// const Portfolio = () => {
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       // Виконайте тут ваші запити або логіку для завантаження даних
-//       await new Promise((resolve) => setTimeout(resolve, 3000)); // Приклад затримки на 3 секунди
-//       setLoading(false); // Після завершення завантаження встановіть стан завантаження в false
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   if (loading) {
-//     return <Loading />;
-//   }
-
+// const Portfolio = async () => {
+//   // await new Promise((resolve) => setTimeout(resolve, 3000));
 //   return (
 //     <section className="portfolio">
 //       <div className="portfolio-wrapper">
@@ -50,3 +15,37 @@ export default Portfolio;
 // };
 
 // export default Portfolio;
+"use client";
+import { useState, useEffect } from "react";
+import PortfolioCategory from "@/components/portfolio-item/PortfolioCategory";
+import Loading from "./loading";
+import "./index.css";
+
+const Portfolio = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <section className="portfolio">
+      <div className="portfolio-wrapper">
+        <PortfolioCategory category="react" title="React" />
+        <PortfolioCategory category="javascript" title="JavaScript" />
+        <PortfolioCategory category="html" title="HTML" />
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
