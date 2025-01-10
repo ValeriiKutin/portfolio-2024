b83900e90ff67f995314be5b646fd920a6056384
Valerii Kutin <valerabulon@gmail.com>
2025-01-10 21:30:01 +0200

    123

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <Providers>
       <html lang="en" suppressHydrationWarning={true}>
-      <meta name="freelancehunt" content="c42defec0c705e7" />
+        <meta name="freelancehunt" content="c42defec0c705e7" />
         <body>
           <ProvidersTheme>
             <NavBar />
